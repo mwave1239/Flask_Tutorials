f5d7c5916fdf6dc84201efb1cb7ac393002ae6e0
Matthew Weideman <mwave@DTmwMBP.local>
2016-07-24 15:12:10 -0400

    Updated Dojo Survey with data validation

--- dojo_survey/server.py
+++ dojo_survey/server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, request, session, flash, url_for
 
 app = Flask(__name__)
+app.secret_key = "SecretKeyDont"
 
 @app.route('/')
 
@@ -10,6 +11,15 @@ def start():
 @app.route('/result', methods=["POST"])
 
 def result():
+    if len(request.form['your_name']) < 1:
+        flash("Name cannot be blank")
+        return redirect('/')
+    elif request.form['comment'] == '':
+        flash("Comments cannot be blank")
+        return redirect('/')
+    elif len(request.form['comment']) > 120:
+        flash("Comments cannot be more than 120 characters")
+        return redirect('/')
     return render_template('submit.html', name = request.form['your_name'], location = request.form['dojo_location'], language = request.form['fav_language'], comment = request.form['comment'] )
 
 app.run(debug=True)
